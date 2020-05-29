16ef3698981d2a74b1ae41b6df5c20859ec0d87a
Ashish Patel <ashpat86@gmail.com>
2020-05-29 17:31:10 -0400

    first commit

---
+++ book.js
@@ -0,0 +1,11 @@
+function Book(title, author, yearPublished){
+    this.title = title;
+    this.author = author;
+    this.yearPublished = yearPublished;
+}
+undefined
+book1 = new Book('The Ballad of Songbirds and Snakes', 'Suzanne Collins', 2020);
+book2 = new Book('The Jane Austen Society: A Novel', 'Natalie Jenner', 2020);
+book3 = new Book('Hollywood Park', 'Mikel Jollett', 2020);
+book4 = new Book('Almond: A Novel', 'Won-pyung Sohn', 2020);
+book5 = new Book('Fire in Paradise: An American Tragedy', 'Alastair Gee', 2020);
